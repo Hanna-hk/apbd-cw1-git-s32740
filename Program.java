a532c1e276f2d41c6d78f6bb2fcfdd1b1472ccc4
s32740 <s32740@pjwstk.edu.pl>
2026-03-12 15:55:29 +0100

    refactor: use for-loop for max value calculation

--- Program.java
+++ Program.java
@@ -53,7 +53,14 @@ public class Program {
         if (numbers == null || numbers.length == 0) {
             throw new IllegalArgumentException("Array cannot be empty");
         }
-        return Arrays.stream(numbers).max().getAsInt();
+
+        int max = numbers[0]; // Zakładamy, że pierwszy element jest największy
+        for (int i = 1; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+        return max;
     }
     public static int findMin(int[] numbers) {
         if (numbers == null || numbers.length == 0) {
